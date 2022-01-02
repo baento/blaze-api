85c6097d231f52620b2af592b35d75c8fc2603ff
baento <antoine.balieu@outlook.com>
2022-01-02 13:38:42 +0100

    Added university component

--- src/main/java/fr/blaze/model/Component.java
+++ src/main/java/fr/blaze/model/Component.java
@@ -1,15 +1,40 @@
 package fr.blaze.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @SuperBuilder
+@NoArgsConstructor
 @Entity
 public class Component extends Resource {
 
+    private String name;
+
+    @ManyToOne
+    private Component parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Component> children;
+
+    private int maxVolumeFI;
+
+    private int maxVolumeFC;
+
+    private int maxVolumeFA;
+
+    private int maxVolumeOther;
+
+    private int reference;
+
+    private int weightedReference;
 }
